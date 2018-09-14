4ee37fd1503221e46cba9b357d480ff49ca97ced
AJIOB <aleksandr9809@gmail.com>
2018-09-14 12:46:26 +0300

    led1 to led3 diodes were added

--- lab3/led.c
+++ lab3/led.c
@@ -6,23 +6,58 @@
   if ((led > LAST_LED)/* || (led < FIRST_LED)*/)\
   {return;}                                     \
 
-static int led_bits[] = 
+static led_info_t led_bits[] = 
 {
-  BIT1,
-  BIT2,
-  BIT3,
-  BIT4,
-  BIT5,
+  { //1.0
+    .dir_port = &P1DIR,
+    .out_port = &P1OUT,
+    .bit_mask = BIT0,
+  },
+  { //8.1
+    .dir_port = &P8DIR,
+    .out_port = &P8OUT,
+    .bit_mask = BIT1,
+  },
+  { //8.2
+    .dir_port = &P8DIR,
+    .out_port = &P8OUT,
+    .bit_mask = BIT2,
+  },
+  { //1.1
+    .dir_port = &P1DIR,
+    .out_port = &P1OUT,
+    .bit_mask = BIT1,
+  },
+  { //1.2
+    .dir_port = &P1DIR,
+    .out_port = &P1OUT,
+    .bit_mask = BIT2,
+  },
+  { //1.3
+    .dir_port = &P1DIR,
+    .out_port = &P1OUT,
+    .bit_mask = BIT3,
+  },
+  { //1.4
+    .dir_port = &P1DIR,
+    .out_port = &P1OUT,
+    .bit_mask = BIT4,
+  },
+  { //1.5
+    .dir_port = &P1DIR,
+    .out_port = &P1OUT,
+    .bit_mask = BIT5,
+  },
 };
 
 void led_init(led_t led)
 {
   LED_CHECK;
 
-  int bit = led_bits[led];
+  led_info_t bit_info = led_bits[led];
 
-  SET_BITS(P1DIR, bit);
-  RESET_BITS(P1OUT, bit);
+  SET_BITS(*bit_info.dir_port, bit_info.bit_mask);
+  RESET_BITS(*bit_info.out_port, bit_info.bit_mask);
 }
 
 void blink_led(led_t led)
@@ -38,21 +73,22 @@ void set_led_state(led_t led, bool state)
 {
   LED_CHECK;
   
-  int bit = led_bits[led];
+  led_info_t bit_info = led_bits[led];
   if (state)
   {
-    SET_BITS(P1OUT, bit);
+    SET_BITS(*bit_info.out_port, bit_info.bit_mask);
   }
   else
   {
-    RESET_BITS(P1OUT, bit);
+    RESET_BITS(*bit_info.out_port, bit_info.bit_mask);
   }
 }
 
 bool get_led_state(led_t led)
 {  
-  int bit = led_bits[led];
-  return ((P1OUT & bit) ? true : false);
+  int bit = led_bits[led].bit_mask;
+  out_port_type_t* out = led_bits[led].out_port;
+  return (((*out) & bit) ? true : false);
 }
 
 void arifm_delay()
--- lab3/types.h
+++ lab3/types.h
@@ -8,15 +8,27 @@ typedef int bool;
 
 typedef enum led_t
 {
-  led4 = 0,
+  led1 = 0,
+  led2,
+  led3,
+  led4,
   led5,
   led6,
   led7,
   led8,
 } led_t;
 
-#define FIRST_LED led4
+#define FIRST_LED led1
 #define LAST_LED led8
+
+typedef unsigned char volatile out_port_type_t;
+
+typedef struct led_info_t
+{
+  out_port_type_t* dir_port;
+  out_port_type_t* out_port;
+  unsigned short bit_mask;
+} led_info_t;
 
 typedef enum ccr_channels_t
 {
