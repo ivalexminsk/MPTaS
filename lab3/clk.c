809dd953aa191e98d187d3d358fff4c9e0084393
AJIOB <aleksandr9809@gmail.com>
2018-09-19 09:53:27 +0300

    Set Vcore up was written

--- lab3/clk.c
+++ lab3/clk.c
@@ -35,6 +35,8 @@ void clk_init()
     SET_BITS(P7SEL, BIT7);
     SET_BITS(P7DIR, BIT7);
 
+    set_vcore_up(1);
+
     //SVS reconfigure to mode, that can work with 01 & 11 in PMMCOREV
     // RESET_BITS(SVSMHCTL, SVSMHRRL_7);
     // SET_BITS(SVSMHCTL, SVSMHRRL_3);
@@ -55,14 +57,18 @@ void clk_toggle()
         SET_BITS(UCSCTL4, SELS_XT1CLK);
 
         //Vcore reconfigure to low-U
-        SET_BITS(PMMCTL0, BIT0);
-        RESET_BITS(PMMCTL0, BIT1);
+        //SET_BITS(PMMCTL0, BIT0);
+        //RESET_BITS(PMMCTL0, BIT1);
+        set_vcore_down(2);
+        set_vcore_down(1);
     }
     else
     {
         //Vcore reconfigure to high-U
-        SET_BITS(PMMCTL0, BIT0);
-        SET_BITS(PMMCTL0, BIT1);
+        //SET_BITS(PMMCTL0, BIT0);
+        //SET_BITS(PMMCTL0, BIT1);
+        set_vcore_up(2);
+        set_vcore_up(3);
 
         //MCLK reconf
         RESET_BITS(UCSCTL4, SELM_MASK);
@@ -75,4 +81,38 @@ void clk_toggle()
 
 
     //TODO:
+}
+
+void set_vcore_up (unsigned int level)
+{
+    // Open PMM registers for write access
+    PMMCTL0_H = 0xA5;
+    //TODO: fix
+    // Make sure no flags are set for iterative sequences
+    // while ((PMMIFG & SVSMHDLYIFG) == 0);
+    // while ((PMMIFG & SVSMLDLYIFG) == 0);
+    // Set SVS/SVM high side new level
+    SVSMHCTL = SVSHE + SVSHRVL0 * level + SVMHE + SVSMHRRL0 * level;
+    // Set SVM low side to new level
+    SVSMLCTL = SVSLE + SVMLE + SVSMLRRL0 * level;
+    // Wait till SVM is settled
+    while ((PMMIFG & SVSMLDLYIFG) == 0);
+    // Clear already set flags
+    RESET_BITS(PMMIFG, (SVMLVLRIFG + SVMLIFG));
+    // Set VCore to new level
+    PMMCTL0_L = PMMCOREV0 * level;
+    // Wait till new level reached
+    if ((PMMIFG & SVMLIFG))
+    {
+        while ((PMMIFG & SVMLVLRIFG) == 0);
+    }
+    // Set SVS/SVM low side to new level
+    SVSMLCTL = SVSLE + SVSLRVL0 * level + SVMLE + SVSMLRRL0 * level;
+    // Lock PMM registers for write access
+    PMMCTL0_H = 0x00;
+}
+
+void set_vcore_down (unsigned int level)
+{
+
 }
--- lab3/clk.h
+++ lab3/clk.h
@@ -2,3 +2,5 @@
 
 void clk_init();
 void clk_toggle();
+void set_vcore_up (unsigned int level);
+void set_vcore_down (unsigned int level);
