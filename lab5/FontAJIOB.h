7fa23db99dabcd378705534fde6f45393245471a
AJIOB <aleksandr9809@gmail.com>
2018-10-16 16:07:41 +0300

    AJIOB Font, based on Fipps, was added

---
+++ lab5/FontAJIOB.h
@@ -0,0 +1,197 @@
+
+/*******************************************************************************
+* font
+* filename: D:/Program Files Portable/lcd-image-converter/projects/FontAJIOB.xml
+* name: FontAJIOB
+* family: Fipps
+* size: 8
+* style: Normal
+* included characters: 0123456789
+* antialiasing: no
+* type: monospaced
+* encoding: CP1251
+* unicode bom: no
+*
+* preset name: Monochrome AJIOB
+* data block size: 8 bit(s), uint8_t
+* RLE compression enabled: no
+* conversion type: Monochrome, Diffuse Dither 128
+* bits per pixel: 1
+*
+* preprocess:
+*  main scan direction: top_to_bottom
+*  line scan direction: backward
+*  inverse: yes
+*******************************************************************************/
+
+#pragma once
+
+#include <stdint.h>
+
+
+
+static const uint8_t image_data_FontAJIOB_0x30[10] = {
+    0x3c, 
+    0x62, 
+    0xdd, 
+    0xdd, 
+    0xdd, 
+    0xdd, 
+    0xdd, 
+    0xe3, 
+    0xfe, 
+    0x7c
+};
+
+static const uint8_t image_data_FontAJIOB_0x31[10] = {
+    0x30, 
+    0x68, 
+    0x64, 
+    0x6c, 
+    0x68, 
+    0x68, 
+    0x68, 
+    0x68, 
+    0x78, 
+    0x70
+};
+
+static const uint8_t image_data_FontAJIOB_0x32[10] = {
+    0x3e, 
+    0x61, 
+    0xdf, 
+    0xdc, 
+    0x6c, 
+    0x32, 
+    0x7d, 
+    0xc1, 
+    0xff, 
+    0xfe
+};
+
+static const uint8_t image_data_FontAJIOB_0x33[10] = {
+    0x3e, 
+    0x61, 
+    0xdf, 
+    0xde, 
+    0x64, 
+    0xdc, 
+    0xde, 
+    0xe1, 
+    0x7f, 
+    0x3e
+};
+
+static const uint8_t image_data_FontAJIOB_0x34[10] = {
+    0x66, 
+    0xdd, 
+    0xdd, 
+    0xdd, 
+    0xc1, 
+    0xdf, 
+    0xde, 
+    0xd0, 
+    0xf0, 
+    0xe0
+};
+
+static const uint8_t image_data_FontAJIOB_0x35[10] = {
+    0x7f, 
+    0xc1, 
+    0xfd, 
+    0x3d, 
+    0x61, 
+    0xdf, 
+    0xde, 
+    0xe1, 
+    0x7f, 
+    0x3e
+};
+
+static const uint8_t image_data_FontAJIOB_0x36[10] = {
+    0x3c, 
+    0x62, 
+    0x7d, 
+    0x1d, 
+    0x61, 
+    0xdd, 
+    0xdd, 
+    0xe3, 
+    0x7e, 
+    0x3c
+};
+
+static const uint8_t image_data_FontAJIOB_0x37[10] = {
+    0x7e, 
+    0xc1, 
+    0xdf, 
+    0xe8, 
+    0x74, 
+    0x34, 
+    0x3a, 
+    0x1a, 
+    0x1e, 
+    0x1c
+};
+
+static const uint8_t image_data_FontAJIOB_0x38[10] = {
+    0x3c, 
+    0x62, 
+    0xdd, 
+    0xdd, 
+    0x63, 
+    0xdd, 
+    0xdd, 
+    0xe3, 
+    0x7e, 
+    0x3c
+};
+
+static const uint8_t image_data_FontAJIOB_0x39[10] = {
+    0x3c, 
+    0x62, 
+    0xdd, 
+    0xdd, 
+    0xc3, 
+    0xde, 
+    0xdc, 
+    0xe2, 
+    0x7e, 
+    0x3c
+};
+
+
+static const uint8_t* FontAJIOB_array[] = {
+
+  // character: '0'
+  image_data_FontAJIOB_0x30,
+
+  // character: '1'
+  image_data_FontAJIOB_0x31,
+
+  // character: '2'
+  image_data_FontAJIOB_0x32,
+
+  // character: '3'
+  image_data_FontAJIOB_0x33,
+
+  // character: '4'
+  image_data_FontAJIOB_0x34,
+
+  // character: '5'
+  image_data_FontAJIOB_0x35,
+
+  // character: '6'
+  image_data_FontAJIOB_0x36,
+
+  // character: '7'
+  image_data_FontAJIOB_0x37,
+
+  // character: '8'
+  image_data_FontAJIOB_0x38,
+
+  // character: '9'
+  image_data_FontAJIOB_0x39
+
+};
+
