e8e4b9d1551e31ce9839d8bf2aa794148792af84
AJIOB <aleksandr9809@gmail.com>
2018-11-01 19:58:36 +0300

    Cma3000 read example was added

--- lab6/main.c
+++ lab6/main.c
@@ -1,9 +1,20 @@
 
+#include <stdio.h>
 #include "driverlib.h"
+#include "HAL_Cma3000.h"
 
 void main( void )
 {
   // Stop watchdog timer to prevent time out reset
   WDTCTL = WDTPW + WDTHOLD;
 
+  Cma3000_init();
+
+  printf("Hello\n");
+
+  while (1)
+  {
+    Cma3000_readAccel();
+    printf("X=%d\tY=%d\tZ=%d\n", Cma3000_xAccel, Cma3000_yAccel, Cma3000_zAccel);
+  }
 }
